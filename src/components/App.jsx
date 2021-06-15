276fa64070c0b36733dc456a390217cea0d112c7
Chirag44518 <chiraggarg.4451@gmail.com>
2021-06-15 17:12:49 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  function addItem(inputText) {
+    setItems(prevItems => {
+      return [...prevItems, inputText];
+    });
+  }
+
+  function deleteItem(id) {
+    setItems(prevItems => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <InputArea onAdd={addItem} />  
+       <div>
+        <ul>
+          {items.map((todoItem, index) => (
+            <ToDoItem
+              key={index}
+              id={index}
+              text={todoItem}
+              onChecked={deleteItem}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+//Flow=> onClick(Add button)->InputArea component Tap on to the property --
+// onAdd(function will get a value inputText) and which will trigger the 
+//addItem(function will get value inputText passed by onAdd prop) function
+//and which will add the item to the items array
+
+     
